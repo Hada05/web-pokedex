4213a8953786e2d31513fe8cc9c086f5f267b7c3
Hada <abinnahada@gmail.com>
2025-08-01 02:10:09 +0700

    feat: add font, made navbar, made basic home

---
+++ app/components/organisms/Navbar.tsx
@@ -0,0 +1,48 @@
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+export default function Navbar() {
+  const logoURL = "/pokemon.svg";
+
+  const [Open, setOpen] = useState(false);
+
+  return (
+    <div className="flex flex-col justify-center items-center absolute top-0">
+      <div className="min-w-screen p-4 bg-primary flex justify-between items-center">
+        <Image src={logoURL} alt="" width={100} height={100}></Image>
+
+        <i
+          className="text-foreground hover:cursor-pointer"
+          onClick={() => setOpen(!Open)}
+        >
+          Menu
+        </i>
+      </div>
+      {Open && (
+        <ul className="text-foreground bg-secondary w-full justify-center items-center text-center flex flex-col gap-1 border-background">
+          <Link href="/" className="w-full">
+            <li className="hover:cursor-pointer hover:bg-tertiary w-full">
+              Home
+            </li>
+          </Link>
+          <Link href={"/pokedex"} className="w-full">
+            <li className="hover:cursor-pointer hover:bg-tertiary w-full">
+              Pokédex
+            </li>
+          </Link>
+          <Link href={"/about"} className="w-full">
+            <li className="hover:cursor-pointer hover:bg-tertiary w-full">
+              About
+            </li>
+          </Link>
+          <Link href={"/contacts"} className="w-full">
+            <li className="hover:cursor-pointer hover:bg-tertiary w-full">
+              Contacts
+            </li>
+          </Link>
+        </ul>
+      )}
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
+import React from 'react'
 
 export default function Home() {
-  return <div>Testing</div>;
+  return (
+    <div className='flex flex-col justify-center items-center'>
+      <div className='flex flex-col items-center gap-8'>
+        <h1 className='text-[3rem] font-extrabold text-shadow-sharp text-shadow-primary'>Welcome, Trainers !</h1>
+        <h1 className='text-[2rem] opacity-75 font-normal'>Whatcha lookin' for?</h1>
+      </div>
+    </div>
+  )
 }
